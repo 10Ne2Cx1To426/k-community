a483bebf68068a6e2db35947d68e2055a3131209
Sena <NseCnaT@users.noreply.github.com>
2021-01-17 22:08:34 +0900

    ユーザーのバリデーションとhomeコントローラーの生成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,27 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+
+  # ユーザー登録のバリデーション
+  validates :nickname, presence: { message: 'ニックネームを入力してください' }
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :firstname
+    validates :lastname
+  end
+
+  validates :birthday, presence: { message: '生年月日を入力してください'}
+
+  with_options presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: '@を含めて入力してください' }, uniqueness: { case_sensitive: false } do
+    validates :email
+  end
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true, format: { with: PASSWORD_REGEX, message:'には英字と数字の両方を含めて設定してください' }, length: { minimum: 6 }, confirmation: true do
+    validates :password
+  end
+
+  validates :password_confirmation, presence: true
 end
